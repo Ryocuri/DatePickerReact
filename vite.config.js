2d4839b60169f12d50f1e741c7b6d838ef32068f
Alexandre <alexandre.hacher@gmail.com>
2026-01-29 17:35:05 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.js'),
+      name: 'ReactDatePickerPlugin',
+      fileName: (format) => `react-datepicker-plugin.${format}.js`
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled into your library
+      external: ['react', 'react-dom'],
+      output: {
+        exports: 'named',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
